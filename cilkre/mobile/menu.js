1382ae96c743710658e39e3aad531edf38a252f7
BuildTools <unconfigured@null.spigotmc.org>
2018-01-27 13:02:46 +0100

    cilkre Mobile 0.1.2?
    Update :D

--- cilkre/mobile/menu.js
+++ cilkre/mobile/menu.js
@@ -1,8 +1,27 @@
+document.getElementById("btn-play").addEventListener("click", function() { showPanel(document.getElementById("levels-panel")); });
+document.getElementById("btn-backtomenu").addEventListener("click", function() { hidePanel(document.getElementById("levels-panel")); });
+
 var allButtons = document.getElementsByClassName("level-button");
 for (i = 0; i < allButtons.length; i++) {
-    allButtons[i].addEventListener("mousedown", function(e) { toLevel(e); })
+    allButtons[i].addEventListener("mousedown", function(e) { toLevel(e); });
 }
 
 function toLevel(e) {
-    window.location = "level" + e.target.id[e.target.id.length - 1] + ".html";
+    if (e.target.id[e.target.id.length - 1] != undefined) {
+        document.getElementById("blackpanel").style.animation = "rightToMiddle .75s ease-in-out 1";
+        setTimeout(function () {
+            window.location = "level" + e.target.id[e.target.id.length - 1] + ".html";
+            document.getElementById("blackpanel").style.animation = "";
+        }, 700);
+    }
+}
+
+function showPanel(selectedPanel) {
+    selectedPanel.style.right = "0";
+    selectedPanel.style.animation = "rightToMiddle .75s ease-in-out 1";
+}
+
+function hidePanel(selectedPanel) {
+    selectedPanel.style.right = "-100%";
+    selectedPanel.style.animation = "middleToRight .75s ease-in-out 1";
 }
