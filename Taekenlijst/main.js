edb519ed04c65d4b8c0eb130ba8005d9a657de4b
Qy <HIHIQY1+git@gmail.com>
2018-04-12 18:21:44 +0200

    Really Updated Taekenlijst to V0.9
    Added a bunch of stuff! Security update, and icon update :D

--- Taekenlijst/main.js
+++ Taekenlijst/main.js
@@ -4,6 +4,7 @@ document.getElementById("btn-add").addEventListener("click", showTaskPanel);
 document.getElementById("btn-close").addEventListener("click", hideTaskPanel);
 document.getElementById("btn-add-task").addEventListener("click", addFromForm);
 document.getElementById("welcomeokbutton").addEventListener("click", welcomeOK);
+document.getElementById("logo").addEventListener("click", debugNotification);
 
 checkWelcome();
 render();
@@ -22,26 +23,17 @@ function render() {
     if (tasks != null) {
         for (i = 0; i < tasks.length; i++) {
             if (tasks[i].priority == "high") {
-                // Add task to visible
-                var listContainer = document.getElementById("list-container");
-                listContainer.innerHTML += "<div class=\"task " + tasks[i].priority + "priority\"><p>" +
-                    tasks[i].name + "</p><span class=\"btn-remove\">X</span></div>";
+                renderTask(tasks[i]);
             }
         }
         for (i = 0; i < tasks.length; i++) {
             if (tasks[i].priority == "mid") {
-                // Add task to visible
-                var listContainer = document.getElementById("list-container");
-                listContainer.innerHTML += "<div class=\"task " + tasks[i].priority + "priority\"><p>" +
-                    tasks[i].name + "</p><span class=\"btn-remove\">X</span></div>";
+                renderTask(tasks[i]);
             }
         }
         for (i = 0; i < tasks.length; i++) {
             if (tasks[i].priority == "low") {
-                // Add task to visible
-                var listContainer = document.getElementById("list-container");
-                listContainer.innerHTML += "<div class=\"task " + tasks[i].priority + "priority\"><p>" +
-                    tasks[i].name + "</p><span class=\"btn-remove\">X</span></div>";
+                renderTask(tasks[i]);
             }
         }
 
@@ -52,23 +44,27 @@ function render() {
     }
 }
 
+function renderTask(task) {
+    // Add task to visible
+    var listContainer = document.getElementById("list-container");
+    listContainer.innerHTML += "<div class=\"task " + task.priority + "priority\"><p>" +
+        task.name + "</p><span class=\"btn-remove\"><img src=\"cross.svg\"></span></div>";
+}
+
 function addTask(name, priority) {
     var newTask = {
         "name": name,
         "priority": priority
     }
-    //console.log(newTask)
-    //console.log("Stringify: " + JSON.stringify(newTask));
 
     var currentTasks = [];
     if (localStorage.getItem("tasks") != null) {
         currentTasks = JSON.parse(localStorage.getItem("tasks"));
     }
     currentTasks.push(newTask);
-    //console.log(currentTasks);
     var tasksStringified = JSON.stringify(currentTasks);
     localStorage.setItem("tasks", tasksStringified);
-    
+
     render();
 }
 
@@ -82,10 +78,8 @@ function removeTask(name) {
         currentTasks = JSON.parse(localStorage.getItem("tasks"));
     }
     var index = -1;
-    for (i = 0; i < currentTasks.length; i++)
-    {
-        if (currentTasks[i].name == name)
-        {
+    for (i = 0; i < currentTasks.length; i++) {
+        if (currentTasks[i].name == name) {
             index = i;
         }
     }
@@ -97,7 +91,7 @@ function removeTask(name) {
 }
 
 function addFromForm() {
-    var taskName = document.getElementById("add-task-name").value;
+    var taskName = getHTMLEncodedString(document.getElementById("add-task-name").value);
     if (taskName.length > 0) {
         var prioritySelect = document.getElementById("add-task-priority");
         var taskPriority = prioritySelect.options[prioritySelect.selectedIndex].text.toLowerCase();
@@ -108,12 +102,15 @@ function addFromForm() {
     else {
         var taskNameElement = document.getElementById("add-task-name");
         taskNameElement.style.backgroundColor = "rgb(225, 75, 75)";
-        setTimeout(function() { taskNameElement.style.backgroundColor = ""; }, 1000);
+        setTimeout(function () { taskNameElement.style.backgroundColor = ""; }, 1000);
     }
 }
 
 function removeFromButton(e) {
     var targetParent = e.target.parentNode;
+    if (targetParent.nodeName == "SPAN") {
+        targetParent = targetParent.parentNode;
+    }
     var title = targetParent.getElementsByTagName("p")[0].innerHTML;
     targetParent.style.opacity = "0";
     removeTask(title);
@@ -133,4 +130,26 @@ function welcomeOK() {
     document.getElementById("welcomepanel").style.transitionDuration = "1s";
     document.getElementById("welcomepanel").style.right = "-100%";
     setTimeout(function () { document.getElementById("welcomepanel").style.display = "none"; }, 1000);
+}
+
+function getHTMLEncodedString(str) {
+    document.getElementById("html-encoder-box").innerHTML = "";
+    document.getElementById("html-encoder-box").innerText = str;
+    return document.getElementById("html-encoder-box").innerHTML;
+}
+
+function debugNotification() {
+    if ("Notification" in window) {
+        if (Notification.permission == "granted") {
+            var notOptions = {
+                body: version,
+                icon: "TaekenLijst_logo512.png",
+                requireInteraction: true
+            }
+            var not = new Notification("Taekenlijst", notOptions);
+        }
+        else {
+            Notification.requestPermission();
+        }
+    }
 }
